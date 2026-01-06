ea0ee93c8aa6d2faa661eadfe836a5758c085140
VaggelisStamatis <vaggelisstam500123@gmail.com>
2026-01-06 03:04:51 +0200

    Updated API and fixed null values

---
+++ src/main/java/gr/meallab/MealResponse.java
@@ -0,0 +1,19 @@
+package gr.meallab.model;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MealResponse {
+       
+    @JsonProperty("meals")
+    private List<Meal> meals;
+
+    // Getters και Setters για να μπορούμε να πάρουμε τη λίστα
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+}
